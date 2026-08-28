91872f0f54d5ca1b854e57a115c523c895762c43
dozapsixoza1 <166742425+dozapsixoza1@users.noreply.github.com>
2026-08-28 23:33:56 +0400

    Update paymentService.js

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -1,14 +1,18 @@
-const { STARS_TO_GRAM_RATE } = require('../config');
+const { getSetting } = require('./settingsService');
 
 // Пакеты пополнения: сколько Stars -> сколько GRAM начислим (с бонусом на крупные пакеты)
-const PACKAGES = [
-  { stars: 50, gram: 50 * STARS_TO_GRAM_RATE },
-  { stars: 150, gram: Math.round(150 * STARS_TO_GRAM_RATE * 1.05) }, // +5%
-  { stars: 500, gram: Math.round(500 * STARS_TO_GRAM_RATE * 1.12) }, // +12%
-];
-
-function getPackage(stars) {
-  return PACKAGES.find((p) => p.stars === stars);
+// Курс берётся динамически из настроек (можно менять из админки без перезапуска).
+function getPackages() {
+  const rate = getSetting('stars_to_gram_rate');
+  return [
+    { stars: 50, gram: Math.round(50 * rate) },
+    { stars: 150, gram: Math.round(150 * rate * 1.05) }, // +5%
+    { stars: 500, gram: Math.round(500 * rate * 1.12) }, // +12%
+  ];
 }
 
-module.exports = { PACKAGES, getPackage };
+function getPackage(stars) {
+  return getPackages().find((p) => p.stars === stars);
+}
+
+module.exports = { getPackages, getPackage };
